0b0da2f7463ae628bed9e837ea0a8f461f134eb9
michael-k-oconnor <m.k.oconnor53@gmail.com>
2018-11-12 12:25:13 -0600

    all test passing

--- src/Bee.js
+++ src/Bee.js
@@ -1,14 +1,13 @@
-var Bee = function() {
+var Bee = function () {
   Grub.call(this);
-  Bee.age = 5;
-  Bee.color = 'yellow';
-  Bee.job = 'keep on growing';
-  console.log(this.eat);
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
 };
 
-  Bee.prototype = Object.create(Grub.prototype);
-  Bee.prototype.constructor = Bee;
-  
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
+
 
 
 
--- src/Grub.js
+++ src/Grub.js
@@ -5,7 +5,7 @@ var Grub = function() {
 };
 
 Grub.prototype.eat = function(){
-
+  
 }
 
 
